b4fb3fe0d51c83966495d92c65ad673f43cfc608
inesgarcia-nervion <ines.garcia@iesnervion.es>
2025-12-19 16:58:51 +0100

    Correción nombre

--- src/test/java/com/tfg/lunaris_backend/LunarisBackendApplicationTests.java
+++ src/test/java/com/tfg/lunaris_backend/LunarisBackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TfgBackendApplicationTests {
+class LunarisBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
