eb9e8b6cf6419511ef9f6b780a7e17b282e3c39b
inesgarcia-nervion <ines.garcia@iesnervion.es>
2025-12-19 16:55:29 +0100

    Commit inicial

---
+++ src/main/java/com/tfg/lunaris_backend/LunarisBackendApplication.java
@@ -0,0 +1,12 @@
+package com.tfg.lunaris_backend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunarisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunarisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfg/lunaris_backend/TfgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.lunaris_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
