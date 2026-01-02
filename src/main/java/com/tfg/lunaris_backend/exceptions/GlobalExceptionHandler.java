29a5366813db42c8118e2b600b7d5bda8a6dd8c5
inesgarcia-nervion <ines.garcia@iesnervion.es>
2026-01-02 14:04:21 +0100

    Arreglo 2 de fallo en exceptión global

--- src/main/java/com/tfg/lunaris_backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/tfg/lunaris_backend/exceptions/GlobalExceptionHandler.java
@@ -7,10 +7,38 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({ BookNotFoundException.class, UserListNotFoundException.class, AuthorNotFoundException.class,
-            GenreNotFoundException.class, ReviewNotFoundException.class, UserNotFoundException.class,
-            SagaNotFoundException.class })
+    @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<String> handleBookNotFoundException(BookNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UserListNotFoundException.class)
+    public ResponseEntity<String> handleUserListNotFoundException(UserListNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(AuthorNotFoundException.class)
+    public ResponseEntity<String> handleAuthorNotFoundException(AuthorNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(GenreNotFoundException.class)
+    public ResponseEntity<String> handleGenreNotFoundException(GenreNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ReviewNotFoundException.class)
+    public ResponseEntity<String> handleReviewNotFoundException(ReviewNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(SagaNotFoundException.class)
+    public ResponseEntity<String> handleSagaNotFoundException(SagaNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
