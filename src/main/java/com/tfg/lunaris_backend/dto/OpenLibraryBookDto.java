c7b87d6ebaf6381d69af58903b796e6ca2bd0d0c
inesgarcia-nervion <ines.garcia@iesnervion.es>
2026-01-09 09:13:15 +0100

    Prueba 2

--- src/main/java/com/tfg/lunaris_backend/dto/OpenLibraryBookDto.java
+++ src/main/java/com/tfg/lunaris_backend/dto/OpenLibraryBookDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
